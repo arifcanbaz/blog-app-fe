cfc2351078ea95ca79dfab3fe46d61735cb94c32
arifcanbaz <arifcanbaz908@gmail.com>
2024-06-11 23:29:47 +0300

    redux and router created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { Provider } from "react-redux";
+import store, { persistor } from "./app/store";
+import { PersistGate } from "redux-persist/integration/react"
+import AppRouter from "./router/AppRouter";
+import { ToastContainer } from "react-toastify";
+
+
 
 function App() {
   return (
-    <div>
-      <p>hello</p>
-    </div>
+    <>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <AppRouter />
+        </PersistGate>
+        
+      </Provider>
+      <ToastContainer/>
+    </>
   );
 }
 
